90609baad1065d0c1655721ce97a2c1d483a4797
Lenilson Nascimento <lenilson.100@gmail.com>
2016-10-06 01:47:18 -0300

    JavaScript Refactor and CSS Transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,19 @@
 (function(w, d){
-  var searchForm = d.querySelector('.search__form'),
-    searchQuery = d.querySelector('.search__city'),
-    cityClose = d.querySelector('.city__close'),
-    axiosInstance = axios.create({
-      baseURL: 'https://query.yahooapis.com/v1/public/',
-      timeout: 10000
-    });
+  w.$ = function (selector) {
+    return d.querySelector(selector);
+  }
+
+  w.$ax = axios.create({
+    baseURL: 'https://query.yahooapis.com/v1/public/',
+    timeout: 10000
+  });
+  
+  var searchForm = $('.search__form'),
+    searchQuery = $('.search__city'),
+    cityClose = $('.city__close');
 
   searchWeather = function () {
-    return axiosInstance.get('/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+searchQuery.value+'") and u="c"&format=json')
+    return $ax.get('/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+searchQuery.value+'") and u="c"&format=json')
       .then(function (result) {
         return result.data.query.results.channel;
       }).catch(function (error) {
@@ -17,16 +22,16 @@
   }
 
   searchCapital = function () {
-    var rio = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Rio de Janeiro") and u="c"&format=json'),
-      sp = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Sao Paulo") and u="c"&format=json'),
-      bh = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Belo Horizonte") and u="c"&format=json'),
-      br = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Brasilia") and u="c"&format=json'),
-      be = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Belem") and u="c"&format=json'),
-      sa = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Salvador") and u="c"&format=json'),
-      cu = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Curitiba") and u="c"&format=json'),
-      fr = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Fortaleza") and u="c"&format=json'),
-      ma = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Manaus") and u="c"&format=json'),
-      jp = axiosInstance.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Joao Pessoa") and u="c"&format=json');
+    var rio = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Rio de Janeiro") and u="c"&format=json'),
+      sp = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Sao Paulo") and u="c"&format=json'),
+      bh = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Belo Horizonte") and u="c"&format=json'),
+      br = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Brasilia") and u="c"&format=json'),
+      be = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Belem") and u="c"&format=json'),
+      sa = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Salvador") and u="c"&format=json'),
+      cu = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Curitiba") and u="c"&format=json'),
+      fr = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Fortaleza") and u="c"&format=json'),
+      ma = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Manaus") and u="c"&format=json'),
+      jp = $ax.get('/yql?q=select location.city, item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text="Joao Pessoa") and u="c"&format=json');
     
     return axios.all([rio, sp, bh, br, be, sa, cu, fr, ma, jp])
       .then(function (result) {
@@ -46,19 +51,18 @@
   searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
     searchWeather().then(function (result) {
-      console.log(result);
-      d.querySelector('.city__header h1').textContent = result.location.city + ', ' +
+      $('.city__header h1').textContent = result.location.city + ', ' +
         result.location.region + ' - Brasil';
-      d.querySelector('.city__header h2').textContent = result.item.condition.temp + 'ºC' + ' ' + 
+      $('.city__header h2').textContent = result.item.condition.temp + 'ºC' + ' ' + 
         result.item.condition.text;
-      d.querySelector('.city__low').textContent = result.item.forecast[0].low + 'º';      
-      d.querySelector('.city__high').textContent = result.item.forecast[0].high + 'º';
-      d.querySelector('.city__wind').textContent = Math.floor(result.wind.speed) + 'km/h';
-      d.querySelector('.city__humidity').textContent = result.atmosphere.humidity + '%';
-      d.querySelector('.city__feels__like').textContent = Math.floor((result.wind.chill -32) * 5 / 9) +
+      $('.city__low').textContent = result.item.forecast[0].low + 'º';      
+      $('.city__high').textContent = result.item.forecast[0].high + 'º';
+      $('.city__wind').textContent = Math.floor(result.wind.speed) + 'km/h';
+      $('.city__humidity').textContent = result.atmosphere.humidity + '%';
+      $('.city__feels__like').textContent = Math.floor((result.wind.chill -32) * 5 / 9) +
        'ºC';
 
-      var cityNext = d.querySelector('.city__next');
+      var cityNext = $('.city__next');
       cityNext.textContent = '';
       for (var i = 1; i < 6; i++) {
         var div = d.createElement('div'),
@@ -70,19 +74,19 @@
         div.appendChild(h2);
         cityNext.appendChild(div);
       }
-      d.querySelector('.header').classList.add('header--open')
-      d.querySelector('.city').classList.remove('city--closed');
+      $('.header').classList.add('header--open')
+      $('.city').classList.remove('city--closed');
     });
   });
 
   cityClose.addEventListener('click', function (event) {
     event.preventDefault();
-    d.querySelector('.header').classList.remove('header--open');
-    d.querySelector('.city').classList.add('city--closed');
+    $('.header').classList.remove('header--open');
+    $('.city').classList.add('city--closed');
   });
 
   w.addEventListener('load', function (event) {
-    var container = d.querySelector('.capital__container');
+    var container = $('.capital__container');
     var column1 = d.createElement('div'),
       column2 = d.createElement('div'),
       header1 = d.createElement('div'),
@@ -98,7 +102,6 @@
     header2.innerHTML = '<p class="capital__min">Min</p><p class="capital__max">Max</p><p class="capital__city>&nbsp;</p>"';
 
     searchCapital().then(function (result) {
-      console.log(result);
       column1.appendChild(header1);
       column2.appendChild(header2);
 
@@ -130,7 +133,7 @@
       container.appendChild(column1);
       container.appendChild(column2);
     }).catch(function (error) {
-      console.log(error);
+      return error;
     });
   });
 
