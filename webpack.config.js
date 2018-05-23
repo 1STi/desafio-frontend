ca5b896742a51a46e73c75d86bbe6ae6cdfeae8e
James da Silva <jamesodas@gmail.com>
2018-05-23 14:42:36 -0300

    O Webpack foi instalado e configurado

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const path = require("path")
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  }
+}
