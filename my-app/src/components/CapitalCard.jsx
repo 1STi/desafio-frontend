dee94d12bf0afd61be9799ae7028dd659a97a79a
Gustavo <gustavo.winiast@gmail.com>
2023-01-19 14:59:03 -0300

    [fix]: Arrumando estilizacao responsiva do TableCapital

--- my-app/src/components/CapitalCard.jsx
+++ my-app/src/components/CapitalCard.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../styles/CapitalCard.css";
-// import { Container } from './styles';
 
 function CapitaCard({ city, max, min }) {
   return (
---
+++ my-app/src/hooks/useMediaQuery.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+function useMediaQuery(query) {
+  const getMatches = (query) => {
+    // Prevents SSR issues
+
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+
+    return false;
+  };
+
+  const [matches, setMatches] = useState(getMatches(query));
+
+  function handleChange() {
+    setMatches(getMatches(query));
+  }
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query);
+
+    // Triggered at the first client-side load and if query changes
+    handleChange();
+
+    // Listen matchMedia
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange);
+    } else {
+      matchMedia.addEventListener("change", handleChange);
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange);
+      } else {
+        matchMedia.removeEventListener("change", handleChange);
+      }
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return matches;
+}
+
+export default useMediaQuery;
